7f803ca45083f3ee2b71f9a02f6cfba8e06e16ad
akhilbolla18 <akhilbolla89@gmail.com>
2025-07-24 12:31:41 +0000

    updated the base url

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://deverp.addiwise.com/api/:path*'
+        destination: 'https://uaterp.addiwise.com/api/:path*'
         // destination: 'https://erp.addiwise.com/api/:path*'
       }
     ];
