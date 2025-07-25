87ddd6637f6ef20df822a0d0006b59543afd0f0b
akhilbolla18 <akhilbolla89@gmail.com>
2025-07-25 10:45:16 +0000

    design & print values are not populating

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
+        // destination: 'https://prderp.addiwise.com/api/:path*'
         destination: 'https://uaterp.addiwise.com/api/:path*'
-        // destination: 'https://uaterp.addiwise.com/api/:path*'
         // destination: 'https://erp.addiwise.com/api/:path*'
       }
     ];
